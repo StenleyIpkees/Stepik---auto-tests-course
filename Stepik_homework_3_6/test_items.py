c0c265cb056ea7879b9c5674427e176faee5b0f7
Stenley Ipkees <fess-alex19@yandex.ru>
2020-01-12 20:04:45 +0300

    run autotests for different interface languages

---
+++ Stepik_homework_3_6/test_items.py
@@ -0,0 +1,15 @@
+import time
+
+from selenium.webdriver.common.by import By
+
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+
+def test_the_basket_button_is_on_the_page(browser):
+    browser.get(link)
+    time.sleep(30)
+    # находим кнопку с классом btn-add-to-basket на странице
+    button_list = browser.find_elements(By.CLASS_NAME, "btn-add-to-basket")
+    # проверяем, что длина списка не равна 0, т.е кнопка найдена
+    assert len(button_list) != 0, "button not found on page {}".format(link)
